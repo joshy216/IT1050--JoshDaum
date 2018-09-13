36f3118329ccdef290c26930262829b96b9ad3da
joshy216 <41481880+joshy216@users.noreply.github.com>
2018-09-12 20:31:09 -0400

    Add files via upload

---
+++ Lab-W1/Lab-W1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_W1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = "Hello World";
+            var myName = "Josh Daum";
+            Console.WriteLine(greeting + " from " + myName + "!");
+            //Console.ReadLine();
+        }
+    }
+}
