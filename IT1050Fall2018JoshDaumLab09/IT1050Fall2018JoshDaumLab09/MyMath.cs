452708fdbffab2d2e79c5b1af338d149b3b9fdec
joshy216 <41481880+joshy216@users.noreply.github.com>
2018-11-01 11:35:45 -0400

    Add files via upload

---
+++ IT1050Fall2018JoshDaumLab09/IT1050Fall2018JoshDaumLab09/MyMath.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050Fall2018JoshDaumLab09
+{
+    public class MyMath
+    {
+        public double x;
+        public double y;
+        public double result;
+
+        public MyMath(double operand1, double operand2)
+        {
+            x = operand1;
+            y = operand2;
+        }
+
+        public void Multiply()
+        {
+            Console.Write(x + " * " + y);
+            result = x * y;
+        }
+
+        public void Divide()
+        {
+            Console.Write(x + " / " + y);
+            result = x / y;
+        }
+
+        public void Add()
+        {
+            Console.Write(x + " + " + y);
+            result = x + y;
+        }
+
+        public void Subtract()
+        {
+            Console.Write(x + " - " + y);
+            result = x - y;
+        }
+
+        public void GetResult()
+        {
+            Console.Write(" = " + result);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ IT1050Fall2018JoshDaumLab09/IT1050Fall2018JoshDaumLab09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050Fall2018JoshDaumLab09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double op1;
+            double op2;
+
+            Console.Write("Enter operand 1: ");
+            op1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter operand 2: ");
+            op2 = Convert.ToDouble(Console.ReadLine());
+
+            var myOutput = new MyMath(op1, op2);
+            myOutput.Subtract();
+            myOutput.GetResult();
+
+            myOutput.Add();
+            myOutput.GetResult();
+
+            myOutput.Divide();
+            myOutput.GetResult();
+
+            myOutput.Multiply();
+            myOutput.GetResult();
+
+        }
+    }
+}
