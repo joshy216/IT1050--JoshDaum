765004ccac231bd5de30f69ddb7935fcc38405a6
joshy216 <41481880+joshy216@users.noreply.github.com>
2018-11-09 13:15:46 -0500

    Add files via upload

---
+++ IT1050Fall2018JoshDaumMidterm/IT1050Fall2018JoshDaumMidterm/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050Fall2018JoshDaumMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.)
+
+            //bool keepLooping = true;
+
+            //while (keepLooping)
+            //{
+            //    Console.WriteLine(keepLooping);
+            //}
+
+
+
+
+            // 2.) 
+
+            //int controlVar1 = 2;
+
+            //do
+            //{
+            //    Console.WriteLine("[" + controlVar1 + "]");
+            //    controlVar1 *= 2;
+            //} while (controlVar1 <= 128);
+
+
+
+
+            // 3.) 
+
+            //string commaChar;
+
+            //for (var controlVar2 = 49; controlVar2 >= 1; controlVar2--)
+            //{
+            //    if (controlVar2 > 1)
+            //    {
+            //        commaChar = ", ";
+            //    }
+            //    else
+            //    {
+            //        commaChar = "";
+            //    }
+            //    Console.Write(controlVar2 + commaChar);
+            //    if (controlVar2 == 1)
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //}
+
+
+
+
+            // 4.) 
+
+            //int controlVar3 = 1;
+
+            //do
+            //{
+            //    Console.Write(controlVar3 + "   ");
+            //    controlVar3 += 2;
+            //} while (controlVar3 <= 21);
+            //Console.WriteLine();
+
+
+
+
+            // 5.)
+
+            // In the original code, the loop is a DO-WHILE, so the condition isn't tested until after the first time through the loop. 
+            // In this WHILE loop, the condition is tested first, and so the code within the loop never executes
+
+            //int n = 8;
+            //int i = 10; // initialize
+
+            //while (i < n) // test condition
+            //{
+            //    Console.Write("*");
+            //    i++; // update!
+            //} 
+
+
+
+
+            // 6.)
+
+            //bool icyRain = false;
+            //bool tornadoWarning = false;
+
+            //if (!icyRain && !tornadoWarning)
+            //{
+            //    Console.WriteLine("Let's go outside!");
+            //}
+
+
+
+
+            // 7.)
+
+            //int countUpTo;
+
+            //for (countUpTo = 5; countUpTo >=1; countUpTo --)
+            //{
+            //    for (int numSpaces = 0; numSpaces <= 5 - countUpTo; numSpaces++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int insideCount = 1; insideCount <= countUpTo; insideCount++)
+            //    {
+            //        Console.Write(insideCount);
+            //    }
+
+                
+            //for (int insideCount = countUpTo - 1; insideCount >= 1; insideCount--)
+            //    {
+            //        Console.Write(insideCount);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+
+
+
+        }
+    }
+}
