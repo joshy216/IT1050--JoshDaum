c79db4d1d1cd9855f1acf9da097913b381a54840
joshy216 <41481880+joshy216@users.noreply.github.com>
2018-11-15 14:48:35 -0500

    Add files via upload

---
+++ IT1050LabW13OOPJoshDaum/IT1050LabW13OOPJoshDaum/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050LabW13OOPJoshDaum
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Speed = 0;
+        }
+
+        public Car(string make, string model, int year, int speed)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Speed = speed;
+            this.Year = year;
+        }
+
+        public int SpeedUp()
+        {
+            this.Speed++;
+            return this.Speed;
+
+        }
+
+        public int SlowDown()
+        {
+            if (this.Speed > 0)
+            {
+                this.Speed--;
+            }
+            return this.Speed;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Year + " " + Make + " " + Model + " is going " + Speed + " MPH.");
+        }
+
+    }
+}
