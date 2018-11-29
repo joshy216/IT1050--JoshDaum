a78cd0bd842406236fc350be76e92b811924a687
joshy216 <41481880+joshy216@users.noreply.github.com>
2018-11-28 20:03:07 -0500

    Add files via upload

--- IT1050Fall2018JoshDaumLabW11/IT1050Fall2018JoshDaumLabW11/Program.cs
+++ IT1050Fall2018JoshDaumLabW11/IT1050Fall2018JoshDaumLabW11/Program.cs
@@ -12,64 +12,34 @@ namespace IT1050Fall2018JoshDaumLabW11
         {
             // 1.)
 
-            //string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
-            //Console.WriteLine(names[2]);
-            //Console.WriteLine(names[4]);
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[4]);
 
 
 
 
             // 2.) 
 
-            //    string[] months = new string[12];
+            string[] months = new string[12];
+                           
+            months[0] = "January";
+            months[1] = "February";
+            months[2] = "March";
+            months[3] = "April";
+            months[4] = "May";
+            months[5] = "June";
+            months[6] = "July";
+            months[7] = "August";
+            months[8] = "September";
+            months[9] = "October";
+            months[10] = "November";
+            months[11] = "December";
 
-            //    for (int setMonth = 0; setMonth <= 11; setMonth++)
-            //    {
-            //        switch (setMonth)
-            //        {
-            //            case 0:
-            //                months[setMonth] = "January";
-            //                break;
-            //            case 1:
-            //                months[setMonth] = "February";
-            //                break;
-            //            case 2:
-            //                months[setMonth] = "March";
-            //                break;
-            //            case 3:
-            //                months[setMonth] = "April";
-            //                break;
-            //            case 4:
-            //                months[setMonth] = "May";
-            //                break;
-            //            case 5:
-            //                months[setMonth] = "June";
-            //                break;
-            //            case 6:
-            //                months[setMonth] = "July";
-            //                break;
-            //            case 7:
-            //                months[setMonth] = "August";
-            //                break;
-            //            case 8:
-            //                months[setMonth] = "September";
-            //                break;
-            //            case 9:
-            //                months[setMonth] = "October";
-            //                break;
-            //            case 10:
-            //                months[setMonth] = "November";
-            //                break;
-            //            case 11:
-            //                months[setMonth] = "December";
-            //                break;
-            //        }
-            //    }
-
-            //    for (int showMonth = 0; showMonth <= 11; showMonth++)
-            //    {
-            //        Console.WriteLine("Month number " + (showMonth + 1) + " is " + months[showMonth]);
-            //    }
+            for (int showMonth = 0; showMonth <= 11; showMonth++)
+            {
+                Console.WriteLine("Month number " + (showMonth + 1) + " is " + months[showMonth]);
+            }
 
 
 
@@ -77,10 +47,11 @@ namespace IT1050Fall2018JoshDaumLabW11
 
             // 3.) 
 
-            //string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
-            //foreach (string season in seasons) {
-            //    Console.WriteLine(season);
-            //}
+            string[] seasons = { "Spring", "Summer", "Fall", "Winter" };
+            foreach (string season in seasons)
+            {
+                Console.WriteLine(season);
+            }
 
 
 
